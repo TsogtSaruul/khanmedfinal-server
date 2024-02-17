2a5d3665aac19c2f212686a9858d55e4184f13b6
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-17 10:39:30 +0800

    Update 33 index.js

--- index.js
+++ index.js
@@ -37,17 +37,17 @@ connectDB();
 const app = express();
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 // middelwares
 // app.use(cors({
 //   origin: "*"
 // }));
-// app.use(cors({}));
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
