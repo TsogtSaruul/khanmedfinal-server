25ee270778a11bc1a52ada1e597ebba2ac715da1
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-17 10:57:24 +0800

    Update 66 index.js

--- index.js
+++ index.js
@@ -29,18 +29,11 @@ import emailRoutes from "./routes/emailRoutes.js";
 // rest object
 const app = express();
 
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-// middelwares
-// app.use(cors({
-//   origin: "*"
-// }));
-app.use(cors());
+app.use(cors({
+    origin: ["https://khanmed-final-client.vercel.app"],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
