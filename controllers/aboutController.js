d332d26902c1f69193a10b00576727c837f078cf
TsogtSaruul <73455126+TsogtSaruul@users.noreply.github.com>
2024-02-17 12:35:17 +0800

    Update 55 aboutController.js

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -10,7 +10,18 @@ export const getAllAboutsController = async (req, res) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    fs.readFile(path.resolve(__dirname, "../data/about.txt"), function(err, data) {
+    // fs.readFile(path.resolve(__dirname, "../data/about.txt"), function(err, data) {
+    //   console.log("data ==========> ", data);
+    //   let about = JSON.parse(data);
+
+    //   res.status(200).send({
+    //     success: true,
+    //     message: "Бүх about жагсаалт",
+    //     about,
+    //   });      
+    // });
+
+    fs.readFile("https://github.com/TsogtSaruul/khanmedfinal-server/blob/main/data/about.txt", function(err, data) {
       console.log("data ==========> ", data);
       let about = JSON.parse(data);
 
@@ -20,16 +31,6 @@ export const getAllAboutsController = async (req, res) => {
         about,
       });      
     });
-
-    // fs.readFile("https://github.com/TsogtSaruul/khanmedfinal-server/blob/main/data/about.txt", function(err, data) {
-    //   let about = JSON.parse(data);
-
-    //   res.status(200).send({
-    //     success: true,
-    //     message: "Бүх about жагсаалт",
-    //     about,
-    //   });      
-    // });
 
   } catch (error) {
     console.log(error);
